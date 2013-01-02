bcb9faf9384270f41ef54bc64ed3f43368687864
ctrlcctrlv <copypaste@bitlegend.com>
2013-01-02 14:50:46 -0500

    All maps were flipped on their sides lol

--- rendermap_image.py
+++ rendermap_image.py
@@ -8,8 +8,8 @@ COLORS = {"wall":(0,0,0,255), "teleporter":(255,0,0,255)}
 im = Image.open(sys.argv[1])
 snake_map = im.load()
 
-height = im.size[0]
-length = im.size[1]
+height = im.size[1]
+length = im.size[0]
 
 output = {}
 output["dimensions"] = (height, length)
@@ -18,10 +18,10 @@ print >> sys.stderr, "Defined colors:", COLORS, "\nMap size:", output["dimension
 for k, v in COLORS.iteritems():
     output[k] = []
     current_line = 0
-    for x in range(0,height):
-        for y in range(0,length):
+    for x in range(0,length):
+        for y in range(0,height):
             p = snake_map[x,y]
             if p == v:
-                output[k].append([x+1, y+1])
+                output[k].append([y+1, x+1])
 
 print(output)
